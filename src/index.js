c892dce850496006951aacace9961d155c2b2212
LeslieJR <l.reyes.ubiqum@gmail.com>
2021-10-15 19:17:12 +0200

    make it better

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 require('./mongo');
 const server = require('./server');
 
-server.listen(server.get('port'), ()=>{
+server.listen(process.env.PORT || server.get('port'), ()=>{
     console.log(`Server running on port: ${server.get('port')}`)
 })
 
